c4e1b7af09393c2e83838ec33fed2fe101523985
peastea <you@example.com>
2023-06-18 10:53:14 +0200

    added filter for loaded signals

--- sigex.py
+++ sigex.py
@@ -31,9 +31,10 @@ def save():
 
 def select_signal():
     idx_selection = lb_signals_loaded.curselection()
+    
     for idx in idx_selection:
-        if signals_loaded[idx] not in signals_selected:
-            signals_selected.append(signals_loaded[idx])
+        if lb_signals_loaded.get(idx) not in signals_selected:
+            signals_selected.append(lb_signals_loaded.get(idx))
     signals_selected_var.set(signals_selected)
 
 def deselect_signal():
@@ -41,6 +42,18 @@ def deselect_signal():
     for idx in sorted(idx_selection, reverse=True):
         del signals_selected[idx]
     signals_selected_var.set(signals_selected)
+
+def filter_signals_loaded(*args):
+    filter_entry = signals_loaded_filter.get()
+    list_to_display = signals_loaded
+    if filter_entry:
+        list_to_display = []
+        for signal in signals_loaded:
+            if filter_entry.lower() in signal.lower():
+                list_to_display.append(signal)
+    signals_loaded_var.set(list_to_display)
+
+
 
 
 
@@ -61,17 +74,23 @@ signals_loaded_var = tk.StringVar(root, signals_loaded)
 signals_selected = []
 signals_selected_var = tk.StringVar(root, signals_selected)
 
+signals_loaded_filter = tk.StringVar(root)
+signals_loaded_filter.trace_add('write', filter_signals_loaded)
+
+tb_filter_signals_loaded = ttk.Entry(root, textvariable=signals_loaded_filter)
+tb_filter_signals_loaded.grid(row=1, column=0)
+
 lb_signals_loaded = tk.Listbox(listvariable=signals_loaded_var, selectmode=tk.MULTIPLE)
-lb_signals_loaded.grid(row=1, column=0, rowspan=4)
+lb_signals_loaded.grid(row=2, column=0, rowspan=4)
 
 lb_signals_selected = tk.Listbox(listvariable=signals_selected_var, selectmode=tk.MULTIPLE)
-lb_signals_selected.grid(row=1, column=2, rowspan=4)
+lb_signals_selected.grid(row=2, column=2, rowspan=4)
 
 btn_select = ttk.Button(text=(">"), command=select_signal)
-btn_select.grid(row=2, column=1, sticky="ns")
+btn_select.grid(row=3, column=1, sticky="ns")
 
 btn_deselect = ttk.Button(text=("<"), command=deselect_signal)
-btn_deselect.grid(row=3, column=1, sticky="ns")
+btn_deselect.grid(row=4, column=1, sticky="ns")
 
 root.mainloop()
 
